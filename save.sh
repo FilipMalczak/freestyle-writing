1dd1c1cc2242737c08bfa881298f84068c8063a6
Filip <filip.malczak@gmail.com>
2020-12-13 21:25:00 +0100

    WIP

---
+++ save.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+# after https://stackoverflow.com/a/6245587/1219585
+  ARG="./$(git rev-parse --abbrev-ref HEAD).md"
+else
+  ARG="$1"
+fi
+
+if [ -z "$2" ]
+then
+  MSG="WIP"
+else
+  MSG="$2"
+fi
+
+git add $ARG
+git commit -m $MSG
+git push
